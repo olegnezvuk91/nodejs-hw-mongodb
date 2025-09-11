eefc4ab00e6623a015ad2bb0ad53762d372b7114
olegnezvuk91 <olegnezvuk@gmail.com>
2025-09-11 23:20:05 +0200

    fix utils1

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getEnvVar } from './utils/env.js';
+import { getEnvVar } from './utils/getEnvVar.js';
 import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(getEnvVar('PORT', '3000'));
