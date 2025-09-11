6c33ef9e4ed63b670b5e6a4ee48dafcb8c8c42ae
olegnezvuk91 <olegnezvuk@gmail.com>
2025-09-11 23:23:23 +0200

    db

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { setupServer } from './server.js';
-import { initMongoConnection } from './utils/getEnvVar.js';
+import { initMongoConnection } from './db/initMongoConnection.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
