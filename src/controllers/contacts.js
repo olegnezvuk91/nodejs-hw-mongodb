83884459f9b6a194dcfd5f82fb5984f37e685324
olegnezvuk91 <olegnezvuk@gmail.com>
2025-09-19 10:27:44 +0200

    red get

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,6 +10,7 @@ import {
 export const getContactsController = async (req, res, next) => {
   const contacts = await getAllContacts();
   res.status(200).json({
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -24,6 +25,7 @@ export const getContactByIdController = async (req, res, next) => {
   }
 
   res.status(200).json({
+    status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
