00f98ab24f3da4f29c1c36ee3f56257ae9538503
olegnezvuk91 <olegnezvuk@gmail.com>
2025-09-22 22:13:35 +0200

    fix

--- src/utils/parseSortParams.js
+++ src/utils/parseSortParams.js
@@ -2,7 +2,7 @@ import { SORT_ORDER } from '../constans/index.js';
 
 const parseSortOrder = (sortOrder) => {
   const isKnownOrder = [SORT_ORDER.ASC, SORT_ORDER.DESC].includes(sortOrder);
-  if (!isKnownOrder) return sortOrder;
+  if (isKnownOrder) return sortOrder;
   return SORT_ORDER.ASC;
 };
 
