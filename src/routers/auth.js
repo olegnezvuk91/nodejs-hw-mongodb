0639a848c6498318b884240a106492add91b91c6
olegnezvuk91 <olegnezvuk@gmail.com>
2025-10-07 20:39:56 +0200

    fix

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -7,7 +7,7 @@ import {
 } from '../controllers/auth.js';
 import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
 import { validateBody } from '../middlewares/validateBody.js';
-import { ctrlWrapper } from '../middlewares/ctrlWrapper.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
